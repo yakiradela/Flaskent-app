b5d208e60c64c5cd72ad90a194c7136a2af7193f
Yakir Adela <yakirade@gmail.com>
2025-05-17 18:55:14 +0300

    Update main.tf

--- flask-app/infra/00-boosttrap/main.tf
+++ flask-app/infra/00-boosttrap/main.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "tf_state" {
-  bucket = var.bucket_name
-  force_destroy = true
-
-  versioning {
-    enabled = true
-  }
+  bucket         = var.bucket_name
+  force_destroy  = true
 
   lifecycle {
     prevent_destroy = true
@@ -15,11 +11,20 @@ resource "aws_s3_bucket" "tf_state" {
   }
 }
 
+# הפרדת גרסתיות למשאב עצמאי לפי Terraform v5
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "block_public" {
   bucket = aws_s3_bucket.tf_state.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
